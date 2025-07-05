1532134826fe490925d21f59166d25bc35b598f5
brutiv <berishashefki472@gmail.com>
2025-07-05 23:22:05 +0200

    Site revamp

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 window.onload = () => {
-    const loadingScreen = document.getElementById("loading-screen");
-    const content = document.getElementById("content");
-  
-    setTimeout(() => {
-      loadingScreen.style.opacity = "0";
-      setTimeout(() => {
-        loadingScreen.style.display = "none";
-        document.body.style.visibility = "visible";
-      }, 1000);
-    }, 1000);
-  };
-  
+    document.body.classList.add("show");
+};
